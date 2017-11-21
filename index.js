f3a5621262c77ded0f39ab34f143286cac694dbc
Kimberley Zell <kimberley.zell@gmail.com>
2017-11-21 09:39:15 -0800

    completed basic requirements

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+
+let updateTime = function updateTime() {
+  let dateToday = new Date();
+  let hours = dateToday.getHours();
+  let minutes = dateToday.getMinutes();
+  let seconds = dateToday.getSeconds();
+  let time = `${hours}:${minutes}:${seconds}`;
+  $('#clock').html(time);
+};
+
+$(document).ready(()=> {
+setInterval(updateTime, 1000);
+});
